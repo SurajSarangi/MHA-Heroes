51f203c71b49057577a23c6a6cd3e1a9d6245e8f
SurajSarangi <surajsarangi1@gmail.com>
2021-04-08 13:20:17 +0530

    controllers added and integrated

---
+++ controllers/heroController.js
@@ -0,0 +1,46 @@
+const Hero = require('../models/hero');
+
+const hero_list = (req, res) => {
+    Hero.find().sort({ createdAt:-1 })
+    .then((result) => {
+        res.render('index', { title : 'Home', heroes : result });
+    })
+    .catch(err => console.log(err));
+};
+
+const hero_add_get = (req, res) => {
+    res.render('create', { title: "Add Hero" });
+};
+
+const hero_add_post = (req, res) => {
+    const hero = new Hero(req.body)
+    hero.save()
+        .then(() => res.redirect('/'))
+        .catch(err => console.log(err));
+};
+
+const hero_details = (req, res) => {
+    const id = req.params.id;
+    Hero.findById(id)
+        .then(result => {
+            res.render('details', { title : result.hname, hero : result });
+        })
+        .catch(err => console.log(err));
+};
+
+const hero_delete = (req, res) => {
+    const id = req.params.id;
+    Hero.findByIdAndDelete(id)
+        .then(result => {
+            res.json({ redirect: '/' });
+        })
+        .catch(err => console.log(err));
+};
+
+module.exports = {
+    hero_list,
+    hero_add_get,
+    hero_add_post,
+    hero_details,
+    hero_delete
+};
--- routes/heroRoutes.js
+++ routes/heroRoutes.js
@@ -1,49 +1,20 @@
 const express = require('express');
-const Hero = require('../models/hero');
+const controllers = require('../controllers/heroController');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    Hero.find().sort({ createdAt:-1 })
-    .then((result) => {
-        res.render('index', { title : 'Home', heroes : result });
-    })
-    .catch(err => console.log(err));
-});
+router.get('/', (req, res) => controllers.hero_list(req,res));
 
-router.get('/about',(req,res)=>{
-    res.render('about', { title : "About" });
-});
+router.get('/about',(req,res) => res.render('about', { title : "About" }));
 
 router.get('/heroes', (req,res) => res.redirect('/'));
 
-router.get('/heroes/add', (req,res)=>{
-    res.render('create', { title: "Add Hero" });
-});
+router.get('/heroes/add', (req,res) => controllers.hero_add_get(req,res));
 
-router.post('/heroes', (req,res) => {
-    const hero = new Hero(req.body)
-    hero.save()
-        .then(() => res.redirect('/'))
-        .catch(err => console.log(err));
-});
+router.post('/heroes', (req,res) => controllers.hero_add_post(req,res));
 
-router.get('/heroes/:id', (req,res) => {
-    const id = req.params.id;
-    Hero.findById(id)
-        .then(result => {
-            res.render('details', { title : result.hname, hero : result });
-        })
-        .catch(err => console.log(err));
-});
+router.get('/heroes/:id', (req,res) => controllers.hero_details(req,res));
 
-router.delete('/heroes/:id', (req,res) => {
-    const id = req.params.id;
-    Hero.findByIdAndDelete(id)
-        .then(result => {
-            res.json({ redirect: '/' });
-        })
-        .catch(err => console.log(err));
-});
+router.delete('/heroes/:id', (req,res) => controllers.hero_delete(req,res));
 
 module.exports = router;
